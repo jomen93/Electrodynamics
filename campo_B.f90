f3e25dd4709605424da870da97166a56343313ef
Johan Méndez <jsmendezh@unal.edu.co>
2020-08-22 01:57:31 -0500

    relaxation model 1.0

--- campo_B.f90
+++ campo_B.f90
@@ -3,28 +3,31 @@ use omp_lib
 
  implicit none
  ! iterators 
- integer :: i,j, icount 
+ integer :: i,j, icount
+ ! auxiliar variables 
+ real :: a,b,new
 
  ! grid parameters
- integer, parameter :: imax = 128
- integer, parameter :: jmax = 128
+ integer, parameter :: imax = 256
+ integer, parameter :: jmax = 256
  ! step 
  real, parameter :: h = 1.0
  ! Constants (7 significant digits)
  real, parameter :: pi = 3.141593
 
  ! arrays (physical variables)
- real, allocatable :: phi(:,:)
+ real,     allocatable :: phi(:,:)
+ real,     allocatable :: field_hx(:,:)
+ real,     allocatable :: field_hy(:,:)
+ real,     allocatable :: field_b(:,:)
  integer,  allocatable :: flag(:,:)
  ! flag is a array to identify points in the grid 
  ! choose which equation use in the grid 
  
  ! iman location 
- integer, parameter :: l1 = 0
- 
- integer, parameter :: i1 = 20
- integer, parameter :: i2 = 75
- integer, parameter :: j1 = 20
+ integer, parameter :: i1 = imax/2 - 20
+ integer, parameter :: i2 = imax/2 + 20
+ integer, parameter :: j1 = jmax/2
 
  ! magnetization 
  real, parameter :: m = 1000
@@ -39,6 +42,9 @@ use omp_lib
  ! allocate flag variables
  allocate(flag(imax,jmax))
  allocate(phi(imax,jmax))
+ allocate(field_b(imax,jmax))
+ allocate(field_hx(imax,jmax))
+ allocate(field_hy(imax,jmax))
 
  ! bottom boundary 
  do i = 1,imax
@@ -86,25 +92,56 @@ use omp_lib
  end do
 
  ! solution of the equation , sobre relaxation method 
- !$omp parallel do
+ ! $omp parallel do
+    do icount = 1, 100000
+     write(*,fmt="(a3,a,t21,f6.2,a)",advance="no") achar(13), "progreso completo:    ", (real(icount)/100000)*100.0," %"
 
-    do icount = 1, 100
      do i = 1, imax
       do j = 1, jmax  
         if (flag(i,j).eq.0)then
-          ! A = 1.0 + 1.0/2.0/float(i)
-
+          a = 1.0 + 1.0/2.0/float(i)
+          b = 1.0 - 1.0/2.0/float(i)
+          new = (a*phi(i+1,j)+b*phi(i-1,j)+phi(i,j+1)+phi(i,j-1))/4.0
+        else if (flag(i,j) == 1 .or. flag(i,j) == 2 .or. flag(i,j) == 3)then
+          new = phi(i,j)
+        else if (flag(i,j) == 4)then
+          new = (4.0*phi(2,j)+phi(1,j+1)+phi(1,j-1))/6.0
+        else if (flag(i,j) == 5)then
+          new = (phi(i+1,j)+phi(i-1,j))/2.0
+        else if (flag(i,j) == 6)then
+          new = (m*h+phi(i,j+1)+phi(i,j-1))/2.0
+        else if (flag(i,j) == 7)then
+          new = (m*h+phi(i,j+1)+phi(i,j-1)+phi(i+1,j)+phi(i-1,j))/4.0
+        else
+          stop "The point isn't indexed"
         end if
+      phi(i,j) = new + alpha*(new-phi(i,j))
       end do
      end do
     end do 
+ ! $omp end parallel do
 
-    
- !$OMP end parallel do
+ ! Magnetic potential save
+ open(10,file = "Mag_pot.dat")
+ write(10,*) phi
+ close(10)
+
+ ! compute the h field
+ do i = 2, imax-1
+  do j = 2, jmax -1
+    field_hx(i,j) = (phi(i+1,j)-phi(i-1,j))/h/2.0
+    field_hy(i,j) = (phi(i,j+1)-phi(i,j-1))/h/2.0
+  end do
+ end do
+ 
+ open(20,file = "field_hx.dat")
+ write(20,*) field_hx
+ close(20)
+ open(20,file = "field_hy.dat")
+ write(20,*) field_hy
+ close(20)
 
  
  write(*,*) alpha
- write(*,*) phi(1,1), flag(1,1)
- print*, "Hello from process: ", OMP_GET_THREAD_NUM()
 
  end program campo_B
---
+++ plots.py
@@ -0,0 +1,26 @@
+import numpy as np 
+import matplotlib.pyplot as plt
+
+size = 256
+
+phi = np.loadtxt("Mag_pot.dat")
+phi = phi.reshape((256,256))
+
+field_hx = np.loadtxt("field_hx.dat")
+field_hx = field_hx.reshape((256,256))
+
+field_hy = np.loadtxt("field_hy.dat")
+field_hy = field_hy.reshape((256,256))
+
+
+
+plt.imshow(phi)
+plt.colorbar()
+plt.savefig("Magnetic_potential")
+plt.show()
+
+plt.imshow(np.sqrt(field_hx**2+field_hy**2))
+plt.colorbar()
+plt.savefig("field")
+plt.show()
+
